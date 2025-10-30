e315ec28b7eb066b04d8a59576f35b9b1b81a4ff
Anuyadav1820 <anukriti445@gmail.com>
2025-10-30 15:29:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-{ 
-    <div id="outer-div">
-<h1 id="inner-div"></h1>
-    <h1 id="heading"></h1>
-<h2 id="sub-heading"></h2>
-<h3 id="third-heading"></h3>
-</div>
-const heading = React.createElement("div", { id: "outer-div" },
-    React.createElement("div", { id: "inner-div" },
-        React.createElement("h1", { id: "heading1" }, "Hello! React Learners"),
-        React.createElement("h1", { id: "heading2" }, "Hello! React Learners"),
-        React.createElement("h1", { id: "heading3" }, "Hello! React Learners"),
-        React.createElement("h1", { id: "heading4" }, "Hello! React Learners")
-    )
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-console.log(heading);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
-} ;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
